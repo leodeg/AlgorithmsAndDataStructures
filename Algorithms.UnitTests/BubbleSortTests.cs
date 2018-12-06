ae46de262d83a9f9edb41d7138b7cfccdafa63f7
RdkDvv <rdkdvv@gmail.com>
2018-12-06 21:50:15 +0300

    Algorithms: add a bubble sort implementation

---
+++ Algorithms.UnitTests/BubbleSortTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Algorithms;
+using NUnit.Framework;
+
+namespace Algorithms.UnitTests
+{
+	[TestFixture]
+	class BubbleSortTests
+	{
+		[Test]
+		[TestCase (new int[] { }, new int[] { })]
+		[TestCase (new int[] { 1 }, new int[] { 1 })]
+		[TestCase (new int[] { 2, 1 }, new int[] { 1, 2 })]
+		[TestCase (new int[] { 2, 4, 5, 1, 10 }, new int[] { 1, 2, 4, 5, 10 })]
+		[TestCase (new int[] { 3, 4, 100, 11, 10, 55, 67 }, new int[] { 3, 4, 10, 11, 55, 67, 100 })]
+		public void BubbleSort_WhenCalled_SortArray (int[] arr, int[] expected)
+		{
+			BubbleSort.bubbleSort (ref arr);
+			Assert.That (arr, Is.EqualTo (expected));
+		}
+
+		[Test]
+		[TestCase (new int[] { }, new int[] { })]
+		[TestCase (new int[] { 1 }, new int[] { 1 })]
+		[TestCase (new int[] { 2, 1 }, new int[] { 1, 2 })]
+		[TestCase (new int[] { 1, 2, 1, 5, 10 }, new int[] { 1, 1, 2, 5, 10 })]
+		[TestCase (new int[] { 2, 4, 5, 1, 10 }, new int[] { 1, 2, 4, 5, 10 })]
+		[TestCase (new int[] { 3, 4, 100, 11, 10, 55, 67 }, new int[] { 3, 4, 10, 11, 55, 67, 100 })]
+		public void BubbleSortOptimized_WhenCalled_SortArray (int[] arr, int[] expected)
+		{
+			BubbleSort.bubbleSortOptimized (ref arr);
+			Assert.That (arr, Is.EqualTo (expected));
+		}
+	}
+}
---
+++ Algorithms/BubbleSort.cs
@@ -0,0 +1,50 @@
+﻿namespace Algorithms
+{
+	public static class BubbleSort
+	{
+		public static void bubbleSort (ref int[] arr)
+		{
+			int i, j;
+			int length = arr.Length - 1;
+
+			for (i = 0; i < length; i++)
+			{
+				for (j = 0; j < length - i; j++)
+				{
+					if (arr[j] > arr[j + 1])
+					{
+						int temp = arr[j];
+						arr[j] = arr[j + 1];
+						arr[j + 1] = temp;
+					}
+				}
+			}
+		}
+
+		public static void bubbleSortOptimized (ref int[] arr)
+		{
+			int i, j;
+			int length = arr.Length - 1;
+			bool swapped;
+
+			for (i = 0; i < length; i++)
+			{
+				swapped = false;
+
+				for (j = 0; j < length - i; j++)
+				{
+					if (arr[j] > arr[j + 1])
+					{
+						int temp = arr[j];
+						arr[j] = arr[j + 1];
+						arr[j + 1] = temp;
+						swapped = true;
+					}
+				}
+
+				// If no two elements that can be swapped, it mean array is already sorted
+				if (!swapped) break;
+			}
+		}
+	}
+}
