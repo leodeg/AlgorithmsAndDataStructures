a67ec7d892fe10bbec2378a226d27b2071e1e241
RdkDvv <rdkdvv@gmail.com>
2018-12-30 20:07:03 +0300

    Solution Engine: add equilibrium problem solution

---
+++ SolutionEngine.UnitTests/EquilibriumTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using NUnit.Framework;
+using SolutionEngine;
+
+namespace SolutionEngine.UnitTests
+{
+	[TestFixture]
+	class EquilibriumTests
+	{
+		#region EquilibriumIndex
+
+		[Test]
+		[TestCase (new int[] { 1 }, 0)]
+		[TestCase (new int[] { 5, 5, 3, 5, 5 }, 2)]
+		[TestCase (new int[] { 1, 7, 3, 6, 5, 6 }, 3)]
+		[TestCase (new int[] { -1, -1, -1, 0, 1, 1 }, 0)]
+		public void EquilibriumIndex_ArrayHasPivotValue_ReturnPivotIndex (int[] n, int expectedIndex)
+		{
+			int result = FindPivotIndex.EquilibriumIndex (n);
+			Assert.That (result, Is.EqualTo (expectedIndex));
+		}
+
+		[Test]
+		[TestCase (new int[] { 1, 2, 3 }, -1)]
+		[TestCase (new int[] { 1, 7, 3 }, -1)]
+		[TestCase (new int[] { 1, 7, 3, 5, 10, 15, 20 }, -1)]
+		public void EquilibriumIndex_ArrayHasNotPivotValue_ReturnMinusOne (int[] n, int expectedIndex)
+		{
+			int result = FindPivotIndex.EquilibriumIndex (n);
+			Assert.That (result, Is.EqualTo (expectedIndex));
+		}
+
+		[Test]
+		[TestCase (new int[] { }, -1)]
+		public void EquilibriumIndex_ArraySizeIsZero_ReturnMinusOne (int[] n, int expectedIndex)
+		{
+			int result = FindPivotIndex.EquilibriumIndex (n);
+			Assert.That (result, Is.EqualTo (expectedIndex));
+		}
+
+		#endregion
+
+		#region EquilibriumIndexOptimized
+
+		[Test]
+		[TestCase (new int[] { 1 }, 0)]
+		[TestCase (new int[] { 5, 5, 3, 5, 5 }, 2)]
+		[TestCase (new int[] { 1, 7, 3, 6, 5, 6 }, 3)]
+		[TestCase (new int[] { -1, -1, -1, 0, 1, 1 }, 0)]
+		public void EquilibriumIndexOptimized_ArrayHasPivotValue_ReturnPivotIndex (int[] n, int expectedIndex)
+		{
+			int result = FindPivotIndex.EquilibriumIndexOptimized (n);
+			Assert.That (result, Is.EqualTo (expectedIndex));
+		}
+
+		[Test]
+		[TestCase (new int[] { 1, 2, 3 }, -1)]
+		[TestCase (new int[] { 1, 7, 3 }, -1)]
+		[TestCase (new int[] { 1, 7, 3, 5, 10, 15, 20 }, -1)]
+		public void EquilibriumIndexOptimized_ArrayHasNotPivotValue_ReturnMinusOne (int[] n, int expectedIndex)
+		{
+			int result = FindPivotIndex.EquilibriumIndexOptimized (n);
+			Assert.That (result, Is.EqualTo (expectedIndex));
+		}
+
+		[Test]
+		[TestCase (new int[] { }, -1)]
+		public void EquilibriumIndexOptimized_ArraySizeIsZero_ReturnMinusOne (int[] n, int expectedIndex)
+		{
+			int result = FindPivotIndex.EquilibriumIndexOptimized (n);
+			Assert.That (result, Is.EqualTo (expectedIndex));
+		}
+
+		#endregion
+	}
+}
---
+++ SolutionEngine/FindPivotIndex.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionEngine
+{
+	public static class FindPivotIndex
+	{
+		/* We define the pivot index as the index where the sum of the numbers 
+		 * to the left of the index is equal to the sum of the numbers to the right of the index.
+		 * If no such index exists, we should return -1. 
+		 * If there are multiple pivot indexes, you should return the left-most pivot index.
+		 * 
+		 * Input: integers in the range [0, 1000]
+		 * Input: nums[i] in the range [-1000, 1000]
+		 * Output: -1 if a pivot is not found, else return number that is a pivot.
+		 */
+		public static int EquilibriumIndex (int[] nums)
+		{
+			int pivot, j;
+			int sumLeft;
+			int sumRight;
+
+			for (pivot = 0; pivot < nums.Length; pivot++)
+			{
+				sumLeft = 0;
+				sumRight = 0;
+
+				for (j = 0; j < pivot; j++)
+				{
+					sumLeft += nums[j];
+				}
+
+				for (j = pivot + 1; j < nums.Length; j++)
+				{
+					sumRight += nums[j];
+				}
+
+				if (sumLeft == sumRight)
+				{
+					return pivot;
+				}
+			}
+			return -1;
+		}
+
+		public static int EquilibriumIndexOptimized (int[] nums)
+		{
+			int pivot, j;
+			int sumRight = nums.Sum();
+			int sumLeft = 0;
+
+			for (pivot = 0; pivot < nums.Length; pivot++)
+			{
+				sumRight -= nums[pivot];
+				if (sumLeft == sumRight)
+				{
+					return pivot;
+				}
+				sumLeft += nums[pivot];
+			}
+
+			return -1;
+		}
+	}
+}
