9ac08fa62a6879994090d4c3be48833b1df515b2
LeoDeg <rdkdvv@gmail.com>
2019-03-18 22:54:03 +0300

    ADDED: Hash-table implementation

---
+++ DataStructures/DataStructures/Hash/HashTable.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.DataStructures.Hash
+{
+	class HashTable
+	{
+		private static int EmptyNode = -1;
+		private static int LazyDeleted = -2;
+		private static int FilledNode = 0;
+
+		private int size;
+		internal int[] Data;
+		internal int[] Flag;
+
+		public HashTable (int size)
+		{
+			this.size = size;
+			Data = new int[size + 1];
+			Flag = new int[size + 1];
+
+			for (int i = 0; i <= size; i++)
+			{
+				Flag[i] = EmptyNode;
+			}
+		}
+
+		public int Size { get { return size; } }
+		public bool IsEmpty { get { return size <= 0; } }
+
+		internal virtual int ComputeHash (int key)
+		{
+			return key % size;
+		}
+
+		internal virtual int ResolveCollision (int index)
+		{
+			return index;
+		}
+
+		internal virtual bool InsertNode (int value)
+		{
+			int hash = ComputeHash (value);
+
+			for (int i = 0; i < size; i++)
+			{
+				if (Flag[hash] == EmptyNode || Flag[hash] == LazyDeleted)
+				{
+					Data[hash] = value;
+					Flag[hash] = FilledNode;
+					return true;
+				}
+
+				hash += ResolveCollision (i);
+				hash %= size;
+			}
+
+			return false;
+		}
+
+		internal virtual bool FindNode (int value)
+		{
+			int hash = ComputeHash (value);
+
+			for (int i = 0; i < size; i++)
+			{
+				if (Flag[hash] == EmptyNode)
+					return false;
+
+				if (Flag[hash] == FilledNode && Data[hash] == value)
+					return true;
+
+				hash += ResolveCollision (i);
+				hash %= size;
+			}
+
+			return false;
+		}
+
+		internal virtual bool DeleteNode (int value)
+		{
+			int hash = ComputeHash (value);
+
+			for (int i = 0; i < size; i++)
+			{
+				if (Flag[hash] == EmptyNode)
+					return false;
+
+				if (Flag[hash] == FilledNode && Data[hash] == value)
+				{
+					Flag[hash] = LazyDeleted;
+					return true;
+				}
+
+				hash += ResolveCollision (i);
+				hash %= size;
+			}
+
+			return false;
+		}
+
+		internal virtual void Print ()
+		{
+			for (int i = 0; i < size; i++)
+			{
+				if (Flag[i] == FilledNode)
+				{
+					Console.WriteLine ("[{0} : {1}]", i, Data[i]);
+				}
+			}
+		}
+
+		public override string ToString ()
+		{
+			StringBuilder builder = new StringBuilder ();
+
+			for (int i = 0; i < size; i++)
+			{
+				if (Flag[i] == FilledNode)
+				{
+					builder.Append ($"[{i} : {Data[i]}]");
+				}
+			}
+
+			return builder.ToString ();
+		}
+	}
+}
---
+++ DataStructures/DataStructures/Hash/HashTableSeparateChaining.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace DataStructures.DataStructures.Hash
+{
+	internal class HashTableSeparateChaining
+	{
+		private class Node
+		{
+			private readonly HashTableSeparateChaining Instance;
+
+			internal int value;
+			internal Node next;
+
+			public Node (HashTableSeparateChaining instance, int value, Node node)
+			{
+				Instance = instance;
+				this.value = value;
+				this.next = node;
+			}
+		}
+
+		private int size;
+		private Node[] Data;
+
+		public HashTableSeparateChaining ()
+		{
+			size = 512;
+			Data = new Node[size];
+
+			for (int i = 0; i < size; i++)
+			{
+				Data[i] = null;
+			}
+		}
+
+		private int ComputeHash (int key)
+		{
+			return key % size;
+		}
+
+		public void Insert (int value)
+		{
+			int hash = ComputeHash (value);
+			Data[hash] = new Node (this, value, Data[hash]);
+		}
+
+		public bool Find (int value)
+		{
+			int hash = ComputeHash (value);
+			Node head = Data[hash];
+
+			while (head != null)
+			{
+				if (head.value == value)
+				{
+					return true;
+				}
+				head = head.next;
+			}
+
+			return false;
+		}
+
+		public bool Delete (int value)
+		{
+			int hash = ComputeHash (value);
+			Node nextNode;
+			Node head = Data[hash];
+
+			if (head != null && head.value == value)
+			{
+				Data[hash] = head.next;
+				return true;
+			}
+
+			while (head != null)
+			{
+				nextNode = head.next;
+				if (nextNode != null || nextNode.value == value)
+				{
+					head.next = nextNode.next;
+					return true;
+				}
+				head = nextNode;
+			}
+			return false;
+		}
+	}
+}
