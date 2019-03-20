ba79c6f5dc59e74621ebc40c131bf58420732e93
LeoDeg <rdkdvv@gmail.com>
2019-03-20 21:23:59 +0300

    ADDED: Graph implementation

---
+++ DataStructures/Algorithms/Searching/DepthFirstTraversal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructures.Graph.AdjacencyList;
+
+namespace DataStructures.Algorithms.Searching
+{
+	internal class DepthFirstTraversal
+	{
+		public void DFS_Stack (Graph.AdjacencyList.Graph graph)
+		{
+			int count = graph.Count;
+			int[] visited = new int[count];
+			int current;
+
+			Stack<int> stack = new Stack<int> ();
+			for (int i = 0; i < count; i++)
+			{
+				visited[i] = 0;
+			}
+
+			visited[0] = 1;
+			stack.Push (0);
+
+			while (stack.Count > 0)
+			{
+				current = stack.Pop ();
+
+				Graph.AdjacencyList.Graph.ANode head = graph.GetNode (current);
+				while (head != null)
+				{
+					if (visited[head.destination] == 0)
+					{
+						visited[head.destination] = 1;
+						stack.Push (head.destination);
+					}
+					head = head.next;
+				}
+			}
+		}
+
+		public void DFS_Recursion (Graph.AdjacencyList.Graph graph, int index, int[] visited)
+		{
+			Graph.AdjacencyList.Graph.ANode head = graph.GetNode (index);
+
+			while (head != null)
+			{
+				if (visited[head.destination] == 0)
+				{
+					visited[head.destination] = 1;
+					DFS_Recursion (graph, head.destination, visited);
+				}
+				head = head.next;
+			}
+		}
+	}
+}
---
+++ DataStructures/DataStructures/Graph/AdjacencyList/Graph.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Graph.AdjacencyList
+{
+	class Graph
+	{
+		public class ANode : IComparable<ANode>
+		{
+			internal int source;
+			internal int destination;
+			internal int cost;
+
+			internal ANode next;
+
+			public ANode (int source, int destination, int cost)
+			{
+				this.source = source;
+				this.destination = destination;
+				this.cost = cost;
+			}
+
+			public int CompareTo (ANode other)
+			{
+				return cost - other.cost;
+			}
+		}
+
+		private class AList
+		{
+			internal ANode head;
+		}
+
+		private int count;
+		private AList[] nodes;
+
+		public Graph (int count)
+		{
+			this.count = count;
+			nodes = new AList[count];
+
+			for (int i = 0; i < count; i++)
+			{
+				nodes[i] = new AList ();
+				nodes[i].head = null;
+			}
+		}
+
+		public int Count { get { return count; } }
+
+		public virtual void AddEdge (int source, int destination, int cost)
+		{
+			ANode node = new ANode (source, destination, cost);
+			node.next = nodes[source].head;
+			nodes[source].head = node;
+		}
+
+		public virtual void AddEdge (int source, int destination)
+		{
+			AddEdge (source, destination, 1);
+		}
+
+		public virtual void AddBiEdge (int source, int destination, int cost)
+		{
+			AddEdge (source, destination, cost);
+			AddEdge (destination, source, cost);
+		}
+
+		public virtual void AddBiEdge (int source, int destination)
+		{
+			AddEdge (source, destination, 1);
+		}
+
+		public ANode GetNode (int index)
+		{
+			return nodes[index].head;
+		} 
+
+		public virtual void Print ()
+		{
+			ANode node;
+
+			for (int i = 0; i < count; i++)
+			{
+				node = nodes[i].head;
+				if (node != null)
+				{
+					Console.WriteLine ("Vertex: {0} is connected to: ", i);
+					while (node != null)
+					{
+						Console.WriteLine (node.destination + " ");
+						node = node.next;
+					}
+					Console.WriteLine ("");
+				}
+			}
+		}
+	}
+}
